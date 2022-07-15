b08e76635a27df440f5888beafe11893b4401bf4
Lucas Mortola da cunha <lucas@allto.digital>
2022-07-15 09:50:00 -0300

    remove settings

--- settings.js
+++ settings.js
@@ -1,4 +1,18 @@
-
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 
 var path = require("path");
 var when = require("when");
@@ -15,24 +29,21 @@ var settings = module.exports = {
     // Blacklist the non-bluemix friendly nodes
     nodesExcludes:[ '66-mongodb.js','75-exec.js','35-arduino.js','36-rpi-gpio.js','25-serial.js','28-tail.js','50-file.js','31-tcpin.js','32-udp.js','23-watch.js' ],
 
-  
+    // Enable module reinstalls on start-up; this ensures modules installed
+    // post-deploy are restored after a restage
     autoInstallModules: true,
 
     // Move the admin UI
     httpAdminRoot: '/red',
 
+    // You can protect the user interface with a userid and password by using the following property
+    // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
+    //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
 
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
     functionGlobalContext: { },
-
-    editorTheme: {
-        projects: {
-            // To enable the Projects feature, set this value to true
-            enabled: true
-        }
-    },
 
     storageModule: require("./pgstorage"),
 
